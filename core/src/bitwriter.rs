934537f053178a2255565c0c40c769caf58aaa59
Cillo-x <58360548+Cillo-x@users.noreply.github.com>
2026-09-04 19:46:35 +0800

    feat: simple bit writer

---
+++ core/src/bitwriter.rs
@@ -0,0 +1,98 @@
+pub struct BitWriter<W: AsMut<[u8]>> {
+    buf: W,
+    current_byte: u8,
+    bits_filled: usize,
+    written: usize,
+}
+
+impl<W: AsMut<[u8]>> BitWriter<W> {
+    pub fn new(buf: W) -> Self {
+        BitWriter {
+            buf,
+            bits_filled: 0,
+            current_byte: 0,
+            written: 0,
+        }
+    }
+
+    pub fn write_bits(&mut self, value: u16, mut n: usize) {
+        while n > 0 {
+            let bits_to_take = core::cmp::min(n, 8 - self.bits_filled);
+            let shift = n - bits_to_take;
+
+            let bits = ((value >> shift) & ((1 << bits_to_take) - 1)) as u8;
+            self.current_byte |= bits << (8 - self.bits_filled - bits_to_take);
+
+            self.bits_filled += bits_to_take;
+            n -= bits_to_take;
+
+            if self.bits_filled == 8 {
+                self.flush_byte();
+            }
+        }
+    }
+
+    fn flush_byte(&mut self) {
+        if self.bits_filled > 0 {
+            self.buf.as_mut()[self.written] = self.current_byte;
+            self.written += 1;
+            self.current_byte = 0;
+            self.bits_filled = 0;
+        }
+    }
+
+    /// Flush the current byte, padding unused bits with `1` (required for JPEG)
+    pub fn flush(&mut self) {
+        if self.bits_filled > 0 {
+            self.current_byte |= (1 << (8 - self.bits_filled)) - 1;
+            self.flush_byte();
+        }
+    }
+
+    pub fn finish(mut self) -> W {
+        self.flush();
+        self.buf
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_smoke() {
+        let mut bwriter = BitWriter::new([0; 8]);
+        bwriter.write_bits(1, 8);
+        bwriter.flush();
+        assert_eq!(bwriter.buf[0], 1)
+    }
+
+    #[test]
+    fn test_part_byte() {
+        let mut bwriter = BitWriter::new([0; 8]);
+        bwriter.write_bits(0b1010, 4);
+        bwriter.flush();
+        assert_eq!(bwriter.buf[0], 0b1010_1111)
+    }
+
+    #[test]
+    fn test_cross_one_byte_write() {
+        let mut bwriter = BitWriter::new([0; 8]);
+        bwriter.write_bits(0b1010, 4);
+        bwriter.write_bits(0, 8);
+        bwriter.flush();
+        assert_eq!(bwriter.buf[0], 0b1010_0000);
+        assert_eq!(bwriter.buf[1], 0b0000_1111);
+    }
+
+    #[test]
+    fn test_cross_two_byte_write() {
+        let mut bwriter = BitWriter::new([0; 8]);
+        bwriter.write_bits(0b1010, 4);
+        bwriter.write_bits(0b1001_1111_0000_101, 15);
+        bwriter.flush();
+        assert_eq!(bwriter.buf[0], 0b1010_1001);
+        assert_eq!(bwriter.buf[1], 0b1111_0000);
+        assert_eq!(bwriter.buf[2], 0b101_11111);
+    }
+}
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -4,6 +4,7 @@ extern crate alloc;
 #[allow(unused)]
 use alloc::vec::Vec;
 
+mod bitwriter;
 mod color;
 mod dct;
 mod error;
@@ -11,6 +12,7 @@ mod quant;
 mod rle;
 mod zigzag;
 
+pub use bitwriter::BitWriter;
 pub use color::{YCbCrImage, rgb_to_ycbcr};
 pub use dct::{BlockI16, BlockU8, Dct, NaiveDct};
 pub use quant::{QC, QY, quant, quant_mut};
