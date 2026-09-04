c45b2095e188f27b92b6cbaf115aafd1a5a6189a
Cillo-x <58360548+Cillo-x@users.noreply.github.com>
2026-09-04 14:34:16 +0800

    feat: add zigzag encode

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -8,7 +8,9 @@ mod color;
 mod dct;
 mod error;
 mod quant;
+mod zigzag;
 
 pub use color::{YCbCrImage, rgb_to_ycbcr};
 pub use dct::{BlockI16, BlockU8, Dct, NaiveDct};
 pub use quant::{QC, QY, quant, quant_mut};
+pub use zigzag::zigzag_enc;
