90e8a9e38d41e3b8c24b855a42175c33077508e5
Cillo-x <58360548+Cillo-x@users.noreply.github.com>
2026-09-05 16:43:17 +0800

    feat: huffman encode

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -16,6 +16,9 @@ mod zigzag;
 pub use bitwriter::BitWriter;
 pub use color::{YCbCrImage, rgb_to_ycbcr};
 pub use dct::{BlockI16, BlockU8, Dct, NaiveDct};
+pub use huffman::{
+    AcHuffmanTable, C_AC_HFT, C_DC_HFT, DcHuffmanTable, Y_AC_HFT, Y_DC_HFT, caculate_size_and_amp,
+};
 pub use quant::{QC, QY, quant, quant_mut};
 pub use rle::{AcPair, rle};
 pub use zigzag::zigzag_scan;
